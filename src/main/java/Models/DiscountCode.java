30437eddfc30087633e519418f9606ea489b3ca8
hamidrezayaghobi <59170724+hamidrezayaghobi@users.noreply.github.com>
2020-04-11 22:28:18 +0430

    Models-v0.1{ -DiscountCode-v0 -write the attribute }

--- src/main/java/Models/DiscountCode.java
+++ src/main/java/Models/DiscountCode.java
@@ -1,4 +1,27 @@
 package Models;
 
+import Models.User.Consumer;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class DiscountCode {
+    private String DiscountId;
+    private Date startDate;
+    private Date endDate;
+    private int discountPercent;
+    private long maximumDiscountAmount;
+    private int discountCount;
+    private ArrayList<Consumer> allowedConsumers = new ArrayList<Consumer>();
+
+    public DiscountCode(String discountId, Date startDate, Date endDate, int discountPercent, long maximumDiscountAmount,
+                        int discountCount, ArrayList<Consumer> allowedConsumers) {
+        DiscountId = discountId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.discountPercent = discountPercent;
+        this.maximumDiscountAmount = maximumDiscountAmount;
+        this.discountCount = discountCount;
+        this.allowedConsumers = allowedConsumers;
+    }
 }
