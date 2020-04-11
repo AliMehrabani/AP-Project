cde8bb2d6a92b4a7149e44c2499b4ac6d4caab87
AliMehrabani <ali1379m@gmail.com>
2020-04-11 20:13:45 +0430

    First push of UML version for phase 0

--- src/main/java/Models/User/User.java
+++ src/main/java/Models/User/User.java
@@ -1,12 +1,27 @@
 package Models.User;
 
+import java.util.ArrayList;
+
 public abstract class User {
+    private static ArrayList<User> allUsers = new ArrayList<User>();
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String eMail;
+    private long phoneNumber;
+    private String password;
 
-    public User() {
-
+    public User(String username, String firstName, String lastName, String eMail, long phoneNumber, String password) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        allUsers.add(this);
     }
 
-    public String getType() {
-        return "";
-    }
+
+
+    public abstract String getType();
 }
