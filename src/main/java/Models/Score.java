ad6e2e9318a7a2d365503c473651c5933f958e62
AliMehrabani <ali1379m@gmail.com>
2020-04-16 16:58:06 +0430

    some good changes!

--- src/main/java/Models/Score.java
+++ src/main/java/Models/Score.java
@@ -3,20 +3,22 @@ package Models;
 import Models.User.User;
 
 public class Score {
-    private User Buyer;
+    private User buyer;
     private int score;
     private Product product;
 
-    public Score() {
-
+    public Score(User buyer, int score, Product product) {
+        this.buyer = buyer;
+        this.score = score;
+        this.product = product;
     }
 
     public User getBuyer() {
-        return Buyer;
+        return buyer;
     }
 
     public void setBuyer(User buyer) {
-        this.Buyer = buyer;
+        this.buyer = buyer;
     }
 
     public int getScore() {
